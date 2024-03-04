0a0eb47650a9dab750714e581927d87fbbeb4d0f
rushiraj070 <rushiraj070@gmail.com>
2024-03-04 08:46:54 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="Sensor based Wafer Fault Detection",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Rushiraj",
+    author_email="rushiraj070@yahoo.com",
     packages=find_packages(),
     install_requires=[],
 )
+
